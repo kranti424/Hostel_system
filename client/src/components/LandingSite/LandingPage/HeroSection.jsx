3d7d948a55a9ad1ae425400b829826a2a7f045f1
unknown <krantidagwar1234@gmail.com>
2024-05-16 01:28:50 +0530

    2nd commit

--- client/src/components/LandingSite/LandingPage/HeroSection.jsx
+++ client/src/components/LandingSite/LandingPage/HeroSection.jsx
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import "./Hero.css";
 
 function HeroSection() {
   return (
     <main className="flex flex-col lg:flex-row-reverse justify-center align-center  text-white text-center">
           <img
-            className="h-1/3 w-1/3 ml-20 mt-20 "
+            className="h-1/2 w-1/3 ml-20 mt-20 "
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/VJTI_College_Quadrangle.jpg/1200px-VJTI_College_Quadrangle.jpg"
           ></img>
       <div className="md:pt-[8%]">
+      <h1 className="cormorant-garamond-semibold">
+          VJTI
+        </h1>
+        <br />
         <h1 className="font-bold text-6xl">
-          VJTI Hostel System
+          Hostel System
         </h1>
         <div className="py-20">
           <Link
